bb6f0892bdbea007461aa5a0c3ca7803017f026a
Janardhan <janardhan.silwal28@gmail.com>
2022-03-11 18:17:29 +0545

    MT25TL01GBB08-ESF flash memory operation library.

---
+++ MT25TL.c
@@ -0,0 +1,361 @@
+/*
+ * MT25TL.c
+ *
+ *  Created on: Aug 13, 2021
+ *      Author: Janardhan (JD)
+ */
+#include "MT25TL.h"
+extern DEV_ID_DATA FM1_ID;
+
+/*
+ * @brief	Erases the Initial 5 Sectors of the memory
+ *
+ * @param	none
+ * @retval	none
+ */
+
+void Memory_Erase() {
+	//ERASE Start
+	Erase_Sector_FM(0, 64);
+	for (uint8_t num = 0; num < 5; num++) {
+		Erase_Sector_FM(FLAG_DATA_ADDRESS + SECT * num, 64);
+		Erase_Sector_FM(SSD_MSN_ADDRESS + SECT * num, 64);
+		Erase_Sector_FM(PX4_MSN_ADDRESS + SECT * num, 64);
+		Erase_Sector_FM(HK_ADDRESS + SECT * num, 64);
+		Erase_Sector_FM(SYS_LOG + SECT * num, 64);
+	}
+	FLAG_DATA_ADDRESS = SECT * 1;
+	PX4_MSN_ADDRESS = SECT * 2;
+	SSD_MSN_ADDRESS = SECT * 203;
+	HK_ADDRESS = SECT * 404;
+	SYS_LOG = SECT * 905;
+	Store_Address_Data_to_Flash();
+}
+
+/*
+ * @brief		erases the specified sector
+ *
+ * @param	Sector_address	address in the sector to be erased
+ * @param	Sector_Size		size of sector to be erased
+ * 					64: 64-KB sector
+ * 					32:	32-KB Sub-Sector
+ * 					4:  4-KB  Sub-Sector
+ *
+ * @retval	none
+ */
+void Erase_Sector_FM(uint32_t Sector_address, uint8_t Sector_Size) {
+	uint8_t command;
+	uint8_t ADDR[4];
+	ADDR[0] = (uint8_t) (Sector_address >> 24 & 0xFF);	// 0x _ _ 00 00 00
+	ADDR[1] = (uint8_t) (Sector_address >> 16 & 0xFF);	// 0x 00 _ _ 00 00
+	ADDR[2] = (uint8_t) (Sector_address >> 8 & 0xFF); 	// 0x 00 00 _ _ 00
+	ADDR[3] = (uint8_t) (Sector_address >> 0 & 0xFF);	// 0x 00 00 00 _ _
+
+	switch (Sector_Size) {
+	case 64: // 64kb sector
+		command = ERASE_4_BYTE_SECTOR & 0xFF; // 0xDC (erase operation command)
+		break;
+	case 32: // 32kb sub-sector
+		command = ERASE_4_BYTE_32KB_SUBSECTOR & 0xFF; // 0x5C (erase operation command)
+		break;
+	case 4: //4kb sub-sector
+		command = ERASE_4_BYTE_4KB_SUBSECTOR & 0xFF; // 0x21 (erase operation command)
+		break;
+	}
+
+	Write_Enable(); // enabling Write mode
+
+	RESET_FMCS();	// Lowering the CS Pin
+
+	///////////////////////////////////////////////////////////////////////////
+	HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);// Sector Erase Command (0xDC)
+	HAL_SPI_Transmit(&FM_SPI, ADDR, 4, 100);// Address to erase the sector of
+	///////////////////////////////////////////////////////////////////////////
+	SET_FMCS();		// Setting CS pin back high
+	HAL_Delay(5);
+	return;
+}
+
+/*
+ *@brief Function to write 1 byte data to specified memory address
+ *
+ *@retval null
+ *
+ *@param address 32-bit address to write data
+ *@param data  8-bit data to be written
+ */
+void Write_Byte(uint32_t address, uint8_t data) {
+	uint8_t command;
+	command = FM_4_BYTE_PAGE_PROGRAM;
+	uint8_t ADDR[4];
+	ADDR[0] = (uint8_t) (address >> 24);		// 0x _ _ 00 00 00
+	ADDR[1] = (uint8_t) (address >> 16);		// 0x 00 _ _ 00 00
+	ADDR[2] = (uint8_t) (address >> 8);		// 0x 00 00 _ _ 00
+	ADDR[3] = (uint8_t) (address >> 0);		// 0x 00 00 00 _ _
+
+	Write_Enable();		// Enabling Write Mode of Flash Memory
+
+	RESET_FMCS();		// Pulling CS pin low
+
+	////////////////////////////////////////////////////////////////////
+	HAL_Delay(1);
+	HAL_SPI_Transmit(&FM_SPI, &command, 1, 100); // Write Operation command 1 byte (0x02)
+	HAL_SPI_Transmit(&FM_SPI, ADDR, 4, 100);		// Address to write data on
+	HAL_SPI_Transmit(&FM_SPI, &data, 1, 100);		// Data to write
+	HAL_Delay(1);
+	////////////////////////////////////////////////////////////////////
+
+	SET_FMCS();			// Pulling CS pin back high
+
+//	Write_Disable(); 	// Disabling the Write Mode of Flash Memory
+	return;
+}
+
+/*
+ *@brief 	Provides the startidng address of specified sector
+ *
+ *@param 	sector_num 	sector number of whose address is needed (MAX value 1023)
+ *
+ *@retval	32-bit starting address of the specified sector
+ *
+ */
+uint32_t GET_Sector_Address(uint16_t sector_num) {
+	uint32_t ADDRESS = 0x00;
+	if (sector_num > 1023) {
+		ADDRESS = 0x03FFFFFF;
+		return ADDRESS;
+	}
+
+	ADDRESS = ADDRESS + (FMSector_size + 1) * sector_num;
+	return ADDRESS;
+}
+
+/*
+ * @brief	Provides Starting Address of Specified 32KB sub-sector
+ *
+ * @param	sector_num	sub-sector number of 32KB sub-sector (max 2047)
+ *
+ * @retval	32-bit starting address of the specified 32KB Sub-Sector
+ *
+ */
+
+uint32_t GET_SubSector_Address_32KB(uint16_t sector_num) {
+	uint32_t ADDRESS = 0x00;
+	if (sector_num > 2047) {
+		ADDRESS = 0x03FFFFFF;
+		return ADDRESS;
+	}
+	ADDRESS = ADDRESS + (FMSubSector_32KB_size + 1) * sector_num;
+	return ADDRESS;
+}
+
+/*
+ * @brief	Provides Starting Address of Specified 4KB sub-sector
+ *
+ * @param	sector_num	sub-sector number of 4KB sub-sector (max 16383)
+ *
+ * @retval	32-bit starting address of the specified 4KB Sub-Sector
+ *
+ */
+
+uint32_t GET_SubSector_Address_4KB(uint16_t sector_num) {
+	uint32_t ADDRESS = 0x00;
+	if (sector_num > 16383) {
+		ADDRESS = 0x03FFFFFF;
+		return ADDRESS;
+	}
+	ADDRESS = ADDRESS + (FMSubSector_4KB_size + 1) * sector_num;
+	return ADDRESS;
+}
+
+/*
+ * @brief	Reads data from specified address of flash memory
+ *
+ * @param	address		Location of the data to be read from Flash
+ *
+ * @retval	data_buffer	8-bit data read from Flash Memory
+ *
+ */
+
+uint8_t Read_Byte(uint32_t address) {
+
+	uint8_t data_buffer;
+	uint8_t command = FM_4_BYTE_READ & 0xFF;
+	uint8_t ADDR[4];
+
+	ADDR[0] = (uint8_t) ((address >> 24) & 0xFF);		// 0x _ _ 00 00 00
+	ADDR[1] = (uint8_t) ((address >> 16) & 0xFF);	// 0x 00 _ _ 00 00
+	ADDR[2] = (uint8_t) ((address >> 8) & 0xFF);		// 0x 00 00 _ _ 00
+	ADDR[3] = (uint8_t) ((address >> 0) & 0xFF);		// 0x 00 00 00 _ _
+
+	RESET_FMCS();	// Pulling the CS pin Low
+	/////////////////////////////////////////////////////////////
+	HAL_Delay(1);
+	HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);	//read data command 0x03
+	HAL_SPI_Transmit(&FM_SPI, ADDR, 4, 100);
+	HAL_SPI_Receive(&FM_SPI, &data_buffer, 1, 100);
+	HAL_Delay(1);
+	/////////////////////////////////////////////////////////////
+	SET_FMCS();		// Pulling the CS pin High
+
+	return data_buffer;	// Returning data read from the Flash Memory
+}
+
+/*
+ * @brief	Reads ID data of the Flash memory, static data programmed at factory
+ *
+ * @param	*pRxBuff	Pointer to the temporary data location
+ * @param	pDEV_STR	pointer to structure containing the Flash memory device ID
+ *
+ * @retval	none
+ *
+ */
+
+void Read_ID(uint8_t *pRxBuff, DEV_ID_DATA *pDEV_STR) {
+	DEV_ID_DATA BUFF;
+	uint8_t command;
+	command = READ_ID & 0xFF;
+
+	RESET_FMCS();		// pulling the cs pin Low
+
+	////////////////////////////////////////////////////////////////////////
+	HAL_Delay(1);
+	HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);	// read ID command (0x9E)
+	HAL_Delay(1);
+	HAL_SPI_Receive(&FM_SPI, pRxBuff, 20, 1000);
+	HAL_Delay(1);
+	/////////////////////////////////////////////////////////////////////////
+	SET_FMCS();			// Pulling the CS Pin High
+
+	BUFF.MFR_ID = *pRxBuff++;
+	BUFF.MEM_TYP = *pRxBuff++;
+	BUFF.MEM_CAPACITY = *pRxBuff++;
+	BUFF.REMAINING_BYTES = *pRxBuff++;
+	BUFF.EXTENDED_DEV_ID = *pRxBuff++;
+	BUFF.DEV_CONF_INFO = *pRxBuff++;
+	for (int i = 0; i < 14; i++) {
+		BUFF.UID_CODE[i] = *pRxBuff++;
+	}
+	*pDEV_STR = BUFF;
+	return;
+}
+
+/*
+ * @brief	Reads and returns the status of flash memory in latched in register
+ * @param	None
+ * @retval	Status 	Flash memory status values in status register
+ */
+int8_t Status_Reg_Read() {
+	uint8_t status;
+	uint8_t command;
+	command = READ_STATUS_REG & 0xFF;
+
+	RESET_FMCS();		//pulling CS pin Low
+	////////////////////////////////////////////////////////
+	HAL_SPI_Transmit(&FM_SPI, &command, 1, 100); // Read Status register command (0x05)
+	HAL_Delay(1);
+	HAL_SPI_Receive(&FM_SPI, &status, 1, 100);
+	HAL_Delay(1);
+	/////////////////////////////////////////////////////////
+	SET_FMCS();			// Pulling CS Pin Back High
+	return status;
+}
+
+/*
+ * @brief	Reads the data bits on the commanded register
+ *
+ * @param	command	command byte specifying which register to read
+ *
+ * @retval	uint16_t data in the register
+ * 				only NON-VOLATILE CONFIGURATION REGISTER HAS 16-bit data
+ * 				other register has 8-bit data
+ */
+uint16_t Read_Register(uint8_t command) {
+	uint16_t reg_data;
+	if (command == READ_NONVOLATILE_CONFIG_REG) {
+		uint8_t data_buf[2];
+
+		RESET_FMCS();
+		HAL_Delay(1);
+		///////////////////////////////////////////////////////////////////////////
+		HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);// Sector Erase Command (0xDC)
+		HAL_SPI_Receive(&FM_SPI, data_buf, 2, 100);	// Address to erase the sector of
+		///////////////////////////////////////////////////////////////////////////
+		HAL_Delay(1);
+		SET_FMCS();
+		reg_data = data_buf[0] << 8 & 0xFF | data_buf[1];
+		return reg_data;
+	} else {
+
+		uint8_t data;
+
+		RESET_FMCS();
+		HAL_Delay(1);
+		///////////////////////////////////////////////////////////////////////////
+		HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);// Sector Erase Command (0xDC)
+		HAL_SPI_Receive(&FM_SPI, data, 1, 100);	// Address to erase the sector of
+		///////////////////////////////////////////////////////////////////////////
+		HAL_Delay(1);
+		SET_FMCS();
+		reg_data = data;
+		return reg_data;
+	}
+
+}
+
+/*
+ * @brief	Transfers 256 bytes of data starting from the given address to provided com port
+ *
+ * @param	Address		32-bit address of Starting memory location
+ * @param	port_num	number indicating the port to which the data is to be transferred to
+ * 				1: PC (via USB)
+ * 				2: OBC (via I2C)
+ *
+ * @retval	none
+ */
+
+void Transfer_Data_256Bytes_FM(uint32_t ADDRESS, uint8_t port_num) {
+	uint8_t i = 0;
+	uint8_t temp;
+	switch (port_num) {
+#ifdef DEBUG_MODE
+#ifdef TRAACE_MODE
+#else
+	case 1:
+		myprintf("\nReading data from Flash\n");
+		for (i = 0; i < 256; i++) {
+			CDC_Transmit_FS(Read_Byte(ADDRESS), 1);
+			ADDRESS++;
+		}
+#ifdef DEBUG_MODE
+#ifdef TRACE_MODE
+#else
+		myprintf("Data Transfer Completed\n");
+#endif
+#endif
+		break;
+#endif
+#endif
+	case 2:
+#ifdef DEBUG_MODE
+#ifdef TRACE_MODE
+#else
+		myprintf("Reading data and transferring to OBC.\n");
+#endif
+#endif
+		for (i = 0; i < 256; i++) {
+			temp = Read_Byte(ADDRESS);
+			HAL_I2C_Slave_Receive(&hi2c1, &temp, 1, 5000);
+			ADDRESS++;
+		}
+#ifdef DEBUG_MODE
+#ifdef TRACE_MODE
+#else
+		myprintf("Data Transfer Completed\n");
+#endif
+#endif
+		break;
+	}
+	return;
+}
+
---
+++ MT25TL.h
@@ -0,0 +1,230 @@
+ /*
+ * MT25TL.h
+ *
+ *  Created on: Aug 12, 2021
+ *      Author: User
+ */
+#pragma once
+
+#ifndef MT25TL_H_
+#define MT25TL_H_
+
+#include <main.h>
+#include "common_func.h"
+extern SPI_HandleTypeDef hspi2;
+
+#define FM_SPI hspi2
+
+//command codes  definition for MT25TL Flash memory
+
+/*COMMAND CODES OF MT25TL START */
+//Software Reset operations
+#define RESET_ENABLE						0x66
+#define RESET_MEMORY						0x99
+
+//READ ID Operations
+#define READ_ID								0x9E
+#define MULTIPLE_IO_READ_ID					0xAF
+#define READ_SERIAL_FLASH_DISO_PARAM		0x5A
+
+//READ memory operation command
+#define READ								0x03
+#define FAST_READ							0x0B
+#define DUAL_OUTPUT_FAST_READ				0x3B
+#define DUAL_IO_FAST_READ					0xBB
+#define QUAD_OUTPUT_FAST_READ				0x6B
+#define QUAD_IO_FAST_READ					0xEB
+#define DTR_FAST_READ						0x0D
+#define DTR_DUAL_OUTPUT_FAST_READ			0x3D
+#define DTR_DUAL_IO_FAST_READ				0xBD
+#define DTR_QUAD_OUTPUT_FAST_READ			0x6D
+#define DTR_QUAD_IO_FAST_READ				0xED
+#define QUAD_IO_WORD_READ					0xE7
+
+//READ memory Operations with 4-Byte Address
+
+#define FM_4_BYTE_READ						0x13
+#define FM_4_BYTE_FAST_READ					0x0C
+#define FM_4_BYTE_DUAL_OUTPUT_FAST_READ		0x3C
+#define FM_4_BYTE_DUAL_IO_FAST_READ			0xBC
+#define FM_4_BYTE_QUAD_OUTPUT_FAST_READ		0x6C
+#define FM_4_BYTE_QUAD_IO_FAST_READ			0xEC
+#define FM_4_BYTE_DTR_FAST_READ				0x0E
+#define FM_4_BYTE_DTR_DUAL_IO_FAST_READ		0xBE
+#define FM_4_BYTE_DTR_QUAD_IO_FAST_READ		0xEE
+
+//WRITE Operations
+
+#define WRITE_ENABLE						0x06
+#define WRITE_DISABLE						0x04
+
+//READ REGISTERS Operations
+
+#define READ_STATUS_REG						0x05
+#define READ_FLAG_STATUS_REG				0x70
+#define READ_NONVOLATILE_CONFIG_REG			0xB5
+#define READ_VOLATILE_CONFIG_REG			0x85
+#define READ_ENHANCED_VOLATILE_CONFIG_REG	0x65
+#define READ_EXTENDED_ADDRESS_REG			0xC8
+#define READ_GENERAL_PURPOSE_READ_REG		0x96
+
+//WRITE REGISTER Operations
+#define WRITE_STATUS_REG					0x01
+#define WRITE_NONVOLATILE_CONFIG_REG		0xB1
+#define WRITE_VOLATILE_CONFIG_REG			0x81
+#define WRITE_ENHANCED_VOLATILE_CONFIG_REG	0x61
+#define WRITE_EXTENDED_ADDRESS_REG			0xC5
+
+//CLEAR FLAG STATES REGISTER Operations
+#define CLEAR_FLAG_STATUS_REG				0X50
+
+//PROGRAM OPERATIONS
+#define PAGE_PROGRAM						0x02
+#define DUAL_INPUT_FAST_PROGRAM				0xA2
+#define EXTENDED_DUAL_INPUT_FAST_PROGRAM	0xD2
+#define QUAD_INPUT_FAST_PROGRAM				0x32
+#define EXTENDED_QUAD_INPUT_FAST_PROGRAM	0x38
+
+//PROGRAM Operation with 4-BYTE address
+#define FM_4_BYTE_PAGE_PROGRAM 						0x12
+#define FM_4_BYTE_QUAD_INPUT_FAST_PROGRAM			0x34
+#define FM_4_BYTE_QUAD_INPUT_EXTENDED_FAST_PROGRAM	0x3E
+
+//ERASE OPERATIONS
+#define ERASE_32KB_SUBSECTOR				0x52
+#define ERASE_4KB_SUBSECTOR					0x20
+#define ERASE_SECTOR						0xD8
+#define ERASE_512Mb_BULK					0xC7
+#define ERASE_512MBBLUK						0x60
+
+//used only for MT25QL01GBB Flash
+#define DIE_ERASE							0xC4
+
+//ERASE Operations with 4-byte address
+#define ERASE_4_BYTE_SECTOR					0xDC
+#define ERASE_4_BYTE_4KB_SUBSECTOR			0x21
+#define ERASE_4_BYTE_32KB_SUBSECTOR			0x5C
+
+//SUSPEND/RESUME Operations
+#define PROGRAM_ERASE_SUSPEND				0x75
+#define PROGRAM_ERASE_RESUME				0x7A
+
+//ONE-TIME PROGRAMMABLE (OTP) operations
+#define READ_OTP_ARRAY						0x4B
+#define PROGRAM_OTP_ARRAY					0x42
+
+//4-BYTE ADDRESS MODE Operations
+#define ENTER_4_BYTE_ADDRESS_MODE			0xB7
+#define EXIT_4_BYTE_ADDRESS_MODE			0xE9
+
+//QUAD PROTOCOL OPERATIONS
+#define ENTER_QUAD_IO_MODE					0x35
+#define RESET_QUAD_IO_MODE					0xF5
+
+//DEEP POWER-DOWN Operations
+#define ENTER_DEEP_POWER_DOWN				0xB9
+#define RELEASE_FROM_DEEP_POWER_DOWN		0xAB
+
+//ADVANCED SECTOR PROTECTION OPERATIONS
+#define READ_SECTOR_PROTECTION				0x2D
+#define PROGRAM_SECTOR_PROTECTION			0x2C
+#define	READ_VOLATILE_LOCK_BITS				0xE8
+#define WRITE_VOLATILE_LOCK_BITS			0xE5
+#define READ_NONVOLATILE_LOCK_BITS			0xE2
+#define WRITE_NONVOLATILE_LOCK_BITS			0xE3
+#define ERASE_NONVOLATILE_LOCK_BITS			0xE4
+#define READ_GLOBAL_FREEZE_BIT				0xA7
+#define WRITE_GLOBAL_FREEZE_BIT				0xA6
+#define READ_PASSWORD						0x27
+#define WRITE_PASSWORD						0x28
+#define UNLOCK_PASSWORD						0x29
+
+//ADVANCED SECTOR PROTECTION Operations with 4-BYTE Address
+#define READ_4_BYTE_VOLATILE_LOCK_BITS		0xE0
+#define WRITE_4_BYTE_VOLATILE_LOCK_BITS		0xE1
+
+//ADVANCED FUNCTION INTERFACE Operations
+
+#define INTERFACE_ACTIVATION				0x9B
+#define CYCLIC_REDUNDANCY_CHECK				0x9B
+#define CHECK_REDUNDANCY_CYCLIC				0x27
+
+/*COMMANDS OF MT25TL END */
+
+//Sectors ADDRESSES for FLASH MEMORY of DIFF SECTOR
+
+#define FMSector0_start 0x00
+#define FMSector_size 0xFFFF
+
+#define FMSubSector0_32KB_start 0x00
+#define FMSubSector_32KB_size 0x7FFF
+
+
+#define FMSubSector0_4KB_start 0x00
+#define FMSubSector_4KB_size 0x0FFF
+
+
+typedef struct {
+	uint8_t MFR_ID;
+	uint8_t MEM_TYP;
+	uint8_t MEM_CAPACITY;
+	uint8_t REMAINING_BYTES;
+	uint8_t EXTENDED_DEV_ID;
+	uint8_t DEV_CONF_INFO;
+	uint8_t UID_CODE[14];
+}DEV_ID_DATA;
+
+/* Functions for FLASH OPERATIONS */
+static void SET_FMCS(){
+	HAL_GPIO_WritePin(FM_CS_GPIO_Port, FM_CS_Pin,GPIO_PIN_SET);
+	HAL_Delay(10);
+}
+
+static void RESET_FMCS(){
+	HAL_GPIO_WritePin(FM_CS_GPIO_Port, FM_CS_Pin, GPIO_PIN_RESET);
+	HAL_Delay(10);
+}
+
+/*
+ *@brief function sets the Flash memory in write enable mode
+ *
+ *@retval null
+ *
+ *@param null
+ *
+ */
+static void Write_Enable(){
+	uint8_t command = WRITE_ENABLE&0xFF;
+		RESET_FMCS();
+		HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);
+		SET_FMCS();
+}
+
+static void Write_Disable(){
+	uint8_t command = WRITE_DISABLE;
+	RESET_FMCS();
+	HAL_SPI_Transmit(&FM_SPI, &command, 1, 100);
+	SET_FMCS();
+}
+
+void Memory_Erase();
+void Erase_Sector_FM(uint32_t Sector_address, uint8_t Sector_Size);
+
+
+void Write_Byte(uint32_t address, uint8_t data);
+uint32_t GET_Sector_Address(uint16_t sector_num);
+uint32_t GET_SubSector_Address_32KB(uint16_t sector_num);
+uint32_t GET_SubSector_Address_4KB(uint16_t sector_num);
+
+uint8_t Read_Byte(uint32_t address);
+void Read_ID(uint8_t *pRxBuff, DEV_ID_DATA *pDEV_STR);
+
+int8_t Status_Reg_Read();
+
+void Transfer_Data_256Bytes_FM(uint32_t ADDRESS, uint8_t port_num);
+
+uint16_t Read_Register(uint8_t command);
+
+
+
+#endif /* MT25TL_H_ */
